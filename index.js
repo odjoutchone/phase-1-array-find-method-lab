133b8e9c99d0091c060eaa2c87b6a2058fe5fe66
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-04-23 13:35:16 -0400

    Adds index,js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+let win = record.find(({result}) => result === 'W');
+    
+if (win){
+    return win.year;
+    }
+    
+else{
+    return undefined
+    }
+}
